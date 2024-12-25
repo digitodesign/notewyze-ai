2a926fe4d3d8f5f6dc5c0d0fa7920ba50493928b
digitodesign <immersiveug@gmail.com>
2024-12-25 06:32:09 +0300

    fix: add proper imports in app/__init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,1 +1,19 @@
+from . import core
+from . import crud
+from . import models
+from . import schemas
+from . import routers
+from . import api
+from . import db
+from . import utils
 
+__all__ = [
+    'core',
+    'crud',
+    'models',
+    'schemas',
+    'routers',
+    'api',
+    'db',
+    'utils'
+]
