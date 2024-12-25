59a31e8ccb94d1e7cfb59af26b96ead6db55e051
digitodesign <immersiveug@gmail.com>
2024-12-25 06:14:40 +0300

    Fix import paths in crud/__init__.py

--- backend/app/crud/__init__.py
+++ backend/app/crud/__init__.py
@@ -1,4 +1,8 @@
-from .user import user
+from .crud_user import user
+from .crud_recording import recording
+from .crud_research import research
+from .crud_study import study
+from .crud_quiz import quiz
 
 # For convenience, import all crud operations here
-__all__ = ["user"]
+__all__ = ["user", "recording", "research", "study", "quiz"]
